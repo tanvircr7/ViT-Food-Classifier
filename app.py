cc103c034022ba6e748ebff3d2c6d4b19118700d
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2025-01-13 03:04:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -87,28 +87,3 @@ if uploaded_file is not None:
 
         # Display prediction time
         st.write(f"Prediction time: {pred_time} seconds")
-
-# ### 4. Gradio app ###
-
-# # Create title, description and article strings
-# title = "Food Classifier 🍕🥩🍣"
-# description = "A Vision Transformer feature extractor computer vision model to classify images of food as pizza, steak or sushi."
-# # article = "Created at [09. PyTorch Model Deployment](https://www.learnpytorch.io/09_pytorch_model_deployment/)."
-
-# # Create examples list from "examples/" directory
-# example_list = [["examples/" + example] for example in os.listdir("examples")]
-
-# # Create the Gradio demo
-# demo = gr.Interface(fn=predict, # mapping function from input to output
-#                     inputs=gr.Image(type="pil"), # what are the inputs?
-#                     outputs=[gr.Label(num_top_classes=3, label="Predictions"), # what are the outputs?
-#                              gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs
-#                     # Create examples list from "examples/" directory
-#                     examples=example_list,
-#                     title=title,
-#                     description=description,
-#                     # article=article
-#                     )
-
-# # Launch the demo!
-# demo.launch()
